3714ffb5ea88363108d0d082f801dba1dab0d1a7
kcselm <kcselm93@gmail.com>
2020-02-04 21:31:08 -0600

    Adding Search Component

--- rest_countries/src/App.js
+++ rest_countries/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Search from './components/Search'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1>Rest Countries</h1>
+      <Search />
     </div>
   );
 }
---
+++ rest_countries/src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <form className="form-inline pt-3 my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="text" placeholder="Search" />
+      <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  )
+}
+
+export default Search
