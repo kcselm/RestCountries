4b31ab133b0a704b0aa7660bf6f216cba243ed5b
kcselm <kcselm93@gmail.com>
2020-03-21 12:46:22 -0500

    Updates

---
+++ precache-manifest.32b90eb805ff4d18856291d4ecd06a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca94cd7ce72de241941f09bcde613709",
+    "url": "/RestCountries/index.html"
+  },
+  {
+    "revision": "f29da7b51e6d9f0d6535",
+    "url": "/RestCountries/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b07e613d61e8bf8c1510",
+    "url": "/RestCountries/static/js/2.2985ecfc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RestCountries/static/js/2.2985ecfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29da7b51e6d9f0d6535",
+    "url": "/RestCountries/static/js/main.67bd13bd.chunk.js"
+  },
+  {
+    "revision": "5ec4e08e404de3168d74",
+    "url": "/RestCountries/static/js/runtime-main.5d069f87.js"
+  }
+]);
